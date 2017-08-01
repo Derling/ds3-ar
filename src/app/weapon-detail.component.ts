1fa5d37062edbf6a7d251ca17ec57e83061b912b
Derling Batista <derling0704@gmail.com>
2017-08-01 03:53:34 -0400

    Started weapon detail view

--- src/app/weapon-detail.component.ts
+++ src/app/weapon-detail.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { WeaponDataService } from './weapon-data.service';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Location }               from '@angular/common';
+import { Location } from '@angular/common';
+import { CLASSES } from './classes';
 import 'rxjs/add/operator/switchMap';
 
 
@@ -13,19 +14,25 @@ import 'rxjs/add/operator/switchMap';
 
 export class WeaponDetailComponent implements OnInit {
   private _weapon = [];
-  weapon : any;
+  private _classes = CLASSES;
+  public currentClass = 0;
+  public weapon : any;
   constructor(private service:WeaponDataService,
   private route: ActivatedRoute, private location: Location) { }
   ngOnInit(): void {
 	    this.route.params
           .switchMap((params: Params) => this.service.getWeapon(params['name']))
           .subscribe(weapon => this.assignWeapon(weapon));
+		console.log(this._classes);
+		console.log(this.currentClass);
   }
   assignWeapon(weapon) : void {
 	  this._weapon.push(weapon);
 	  this.weapon = this._weapon[0];
   }
-  debug(weapon) {
-	 console.log(this.weapon);
-  }	 
+  de(){
+	  console.log(this.currentClass);
+	  this.currentClass = 0;
+	  console.log(this.currentClass);
+  }
 }
