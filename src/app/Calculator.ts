d7d4f1fbd92899572ebceaedd718a534c9bdf6a9
Derling Batista <derling0704@gmail.com>
2017-08-02 00:27:42 -0400

    Designing weapon-detail view

--- src/app/Calculator.ts
+++ src/app/Calculator.ts
@@ -1,59 +1,96 @@
-import { Weapon } from './Weapon';
-import { SATURATION } from './saturation-index.service';
+import { SATURATION } from './newSaturation.service';
 
-export class Calculator {
-  saturation = SATURATION;
-  bonusPhysical = 0;
-  bonusMagic = 0;
-  bonusLightning = 0;
-  bonusFire = 0;
-  bonusDark = 0;
-  bonusLuck = 0;
-  totalDmg = 0;
-  constructor(public weapon: Weapon, public stats: {}) { this.init(); }
-    private init(): void {
-      this.calculateBonuses();
-      this.calculateTotalDmg();
-    }
-    private calculateBonuses(): void {
-    this.calculateBonusPhysical();
-    this.calculateBonusMagic();
-    this.calculateBonusLightning();
-    this.calculateBonusFire();
-    this.calculateBonusDark();
-    this.calculateBonusLuck();
-  }
-  private calculateTotalDmg(): void {
-    this.totalDmg = Math.floor(this.weapon.getTotalBaseDmg() + this.bonusPhysical +
-      this.bonusMagic + this.bonusFire + this.bonusLightning + this.bonusDark +
-      this.bonusLuck);
-  }
-  private calculateBonusLuck(): void {
-     this.bonusLuck = +this.weapon.getBaseDmgs()['Physical'] * ((+this.weapon.getScaling()['Luck'])/100 * this.getSaturation(this.weapon.getIndexes()['Physical'],this.stats['Luck'])/100);
-  }
-  private calculateBonusDark(): void {
-    this.bonusDark =  +this.weapon.getBaseDmgs()['Dark'] * ((+this.weapon.getScaling()['Int'])/100 * this.getSaturation(this.weapon.getIndexes()['Dark'],this.stats['Int'])/100 +
-        (+this.weapon.getScaling()['Faith'])/100 * this.getSaturation(this.weapon.getIndexes()['Dark'],this.stats['Faith'])/100);
-  }
-  private calculateBonusFire(): void {
-    this.bonusFire = +this.weapon.getBaseDmgs()['Fire'] * ((+this.weapon.getScaling()['Int'])/100 * this.getSaturation(this.weapon.getIndexes()['Fire'],this.stats['Int'])/100 +
-        (+this.weapon.getScaling()['Faith'])/100 * this.getSaturation(this.weapon.getIndexes()['Fire'],this.stats['Faith'])/100);
-  }
-  private calculateBonusLightning(): void {
-    this.bonusLightning = +this.weapon.getBaseDmgs()['Lightning'] * (+this.weapon.getScaling()['Faith'])/100 * this.getSaturation(this.weapon.getIndexes()['Lightning'],this.stats['Faith'])/100;
-  }
-  private calculateBonusMagic(): void {
-    this.bonusMagic = +this.weapon.getBaseDmgs()['Magic'] * ((+this.weapon.getScaling()['Int'])/100 * this.getSaturation(this.weapon.getIndexes()['Magic'],this.stats['Int'])/100)
-  }
-  private calculateBonusPhysical(): void {
-    this.bonusPhysical = +this.weapon.getBaseDmgs()['Physical'] * ((+this.weapon.getScaling()['Str'])/100 * this.getSaturation(this.weapon.getIndexes()['Physical'],this.stats['Str'])/100 +
-        (+this.weapon.getScaling()['Dex'])/100 * this.getSaturation(this.weapon.getIndexes()['Physical'],this.stats['Dex'])/100);
-  }
-  private getSaturation(index: string, level: string): number {
-    for(const i of this.saturation) {
-      if(i['Curve Index'] == index) {
-        return +i['Level ' + level];
-      }
-    }
-  }
-}
+
+export class Calculator{
+	saturation = SATURATION;
+	totalDmg = 0;
+	bonusPhysical = 0;
+	bonusMagic = 0;
+	bonusLightning = 0;
+	bonusFire = 0;
+	bonusDark = 0;
+	bonusLuck = 0;	
+	constructor(public weapon: {},public infusion:string,
+	public stats: {}) { this.init(); };
+
+	private init() : void {
+		this.calculateBonuses();
+		this.calculateTotalDmg();
+	};
+	
+	private calculateBonuses() : void {
+		this.calculatePhysicalBonus();
+		this.calculateMagicBonus();
+		this.calculateFireBonus();
+		this.calculateLightningBonus();
+		this.calculateDarkBonus();
+		this.calculateLuckBonus();
+	};
+
+	private calculatePhysicalBonus() : void {
+		this.bonusPhysical = 
+			this.weapon['base_damages'][this.infusion]['physical'] *
+			((this.weapon['scaling_coeff'][this.infusion][0]/100 * 
+			this.getSaturation(this.weapon['saturation_index'][this.infusion]['physical'],this.stats['str'])/100) +
+			(this.weapon['scaling_coeff'][this.infusion][1]/100 * 
+			this.getSaturation(this.weapon['saturation_index'][this.infusion]['physical'],this.stats['dex'])/100));
+			if(this.infusion == 'Blessed') {
+				this.bonusPhysical += 
+				this.weapon['base_damages'][this.infusion]['physical'] *
+				((this.weapon['scaling_coeff'][this.infusion][3]/100 * 
+				this.getSaturation(this.weapon['saturation_index'][this.infusion]['physical'],this.stats['faith'])/100));
+			}
+	};
+
+	private calculateLuckBonus() : void {
+		this.bonusLuck =
+			this.weapon['base_damages'][this.infusion]['physical'] *
+			(this.weapon['scaling_coeff'][this.infusion][4]/100 *
+			this.getSaturation(this.weapon['saturation_index'][
+			this.infusion]['physical'],this.stats['luck'])/100);
+	};
+
+	private calculateMagicBonus() : void {
+		this.bonusMagic = 
+			this.weapon['base_damages'][this.infusion]['magic'] *
+			((this.weapon['scaling_coeff'][this.infusion][2]/100 * 
+			this.getSaturation(this.weapon['saturation_index'][this.infusion]['magic'],this.stats['int'])/100));
+	};
+
+	private calculateFireBonus() : void {
+		this.bonusFire = 
+			this.weapon['base_damages'][this.infusion]['fire'] *
+			((this.weapon['scaling_coeff'][this.infusion][2]/100 * 
+			this.getSaturation(this.weapon['saturation_index'][this.infusion]['fire'],this.stats['int'])/100) +
+			(this.weapon['scaling_coeff'][this.infusion][3]/100 * 
+			this.getSaturation(this.weapon['saturation_index'][this.infusion]['fire'],this.stats['faith'])/100));
+	};
+
+	private calculateLightningBonus() : void {
+		this.bonusLightning = 
+			this.weapon['base_damages'][this.infusion]['lightning'] *
+			((this.weapon['scaling_coeff'][this.infusion][3]/100 * 
+			this.getSaturation(this.weapon['saturation_index'][this.infusion]['lightning'],this.stats['faith'])/100))
+	};
+
+	private calculateDarkBonus() : void {
+		this.bonusDark = 
+			this.weapon['base_damages'][this.infusion]['dark'] *
+			((this.weapon['scaling_coeff'][this.infusion][2]/100 * 
+			this.getSaturation(this.weapon['saturation_index'][this.infusion]['dark'],this.stats['int'])/100) +
+			(this.weapon['scaling_coeff'][this.infusion][3]/100 * 
+			this.getSaturation(this.weapon['saturation_index'][this.infusion]['dark'],this.stats['faith'])/100));
+	};
+
+	private getSaturation(index : number,level : number) {
+		for(const i of this.saturation) {
+			if(i['Curve Index'] == index) {
+				return +i['Level ' + level];
+			};
+		};
+	};
+
+	private calculateTotalDmg() : void {
+		this.totalDmg = Math.floor(this.weapon['base_damages'][this.infusion]['physical'] + this.weapon['base_damages'][this.infusion]['magic'] + this.weapon['base_damages'][this.infusion]['fire'] + this.weapon['base_damages'][this.infusion]['lightning'] + this.weapon['base_damages'][this.infusion]['dark'] + this.bonusPhysical + this.bonusMagic + this.bonusFire + this.bonusLightning + this.bonusDark + this.bonusLuck);
+	};
+};
--- src/app/weapon-detail.component.ts
+++ src/app/weapon-detail.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { CLASSES } from './classes';
 import 'rxjs/add/operator/switchMap';
+import { Calculator } from './calculator';
 
 
 
@@ -16,23 +17,33 @@ export class WeaponDetailComponent implements OnInit {
   private _weapon = [];
   private _classes = CLASSES;
   public currentClass = 0;
-  public weapon : any;
+  public weapon : {};
+  public currentLevel: number;
+  public stats : {};
+  infusion = 'Normal';
   constructor(private service:WeaponDataService,
   private route: ActivatedRoute, private location: Location) { }
   ngOnInit(): void {
 	    this.route.params
           .switchMap((params: Params) => this.service.getWeapon(params['name']))
           .subscribe(weapon => this.assignWeapon(weapon));
-		console.log(this._classes);
-		console.log(this.currentClass);
+		this.update();
   }
   assignWeapon(weapon) : void {
 	  this._weapon.push(weapon);
 	  this.weapon = this._weapon[0];
   }
-  de(){
-	  console.log(this.currentClass);
-	  this.currentClass = 0;
-	  console.log(this.currentClass);
+  change(stat : string, increment : number){
+	  this.stats[stat] = this.stats[stat] + increment;
+	  this.currentLevel += increment;
+	  console.log(new Calculator(this.weapon,this.infusion,this.stats));
+  }
+  update(){
+	  this.currentLevel = this._classes[this.currentClass].level;
+	  this.stats = {str : this._classes[this.currentClass].str,
+				dex : this._classes[this.currentClass].dex,
+				int : this._classes[this.currentClass].int,
+				faith : this._classes[this.currentClass].faith,
+				luck : this._classes[this.currentClass].luck};
   }
 }
